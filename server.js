fb2fe861ba06d3747ef2cbcaf261267015e69838
Санджиев Савр Игоревич <sandzhiev_si@bdo.ru>
2019-11-18 18:13:05 +0300

    init

---
+++ server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const https = require('https');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+const server = http.createServer();
+
+const url = 'https://www.flipkart.com/';
+
+nightmare
+  .goto(url)
+  .wait('body')
+  // .click('button._2ISNhP ._29YdH8')
+  .type('input.LM6RPg', 'nodejs books')
+  .click('button.vh79eN')
+  .wait('div.bhgxx2')
+  .evaluate(() => document.querySelector('body').innerHTML)
+  .end()
+  .then(response => {
+    console.log(getData(response));
+  }).catch(err => {
+    console.log(err);
+  });
+
+let getDaa = html => {
+  data = [];
+  const $ = cheerio.load(html);
+  $('div.._1HmYoV._35HD7C:nth-child(2) div.bhgxx.col-12-12').each((row, raw_element) => {
+    $$(raw)
+  })
+}
+// axios.get(url)
+//   .then(response => {
+//     console.log(response.data);
+//     let getData = html => {
+//       data = [];
+//       const $ = cheerio.load(html);
+//       $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+//         data.push({
+//           title: $(elem).text(),
+//           link: $(elem).find('a.storylink').attr('href')
+//         });
+//       });
+//       console.log(data);
+//     };
+//     getData(response.data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
